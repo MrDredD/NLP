d8e2991a7aa6eece97135808d88b8c26116a5eec
Aziz <mrdreddmail@gmail.com>
2019-05-14 02:18:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+from preprocess import preprocessing
+from compute_result import compute_tfidf
+from get_corpus import get_corpus
+
+
 def main():
+    DOCUMENT_TO_PROCESS = "001.htm"
+    doc = preprocessing(DOCUMENT_TO_PROCESS)
+    keywords = compute_tfidf(get_corpus(), doc, 5)
+    print("\nAbstract:")
+    print(doc)
+    print("\nKeywords:")
+    for k in keywords:
+        print(k, keywords[k])
     return
+
+
 if __name__ == '__main__':
     main()
