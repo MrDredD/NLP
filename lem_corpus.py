adaada685032bb9c64bc283504641a88374af8f9
Aziz <mrdreddmail@gmail.com>
2019-05-14 21:42:42 +0300

    Update lem_corpus.py

--- lem_corpus.py
+++ lem_corpus.py
@@ -1,10 +1,10 @@
 import nltk
 # nltk.download("stopwords")
 """
-Creates a corpus of data/
+Creates a corpus of data.
 This programs reads all paths in data directory and all files in subdirectories.
 After that script normalize and preprocess all documents to create a corpus file.
-It will take ~40-60 min to complete this script
+It will take ~40-60 min to complete this script.
 """
 from pymystem3 import Mystem
 from nltk.corpus import stopwords
