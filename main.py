b0aa75fa8fc3956f2ca7754d375a352e4518ca56
Aziz <mrdreddmail@gmail.com>
2019-05-16 16:14:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from preprocess import preprocessing
 from compute_result import compute_tfidf
 from get_corpus import get_corpus
+from get_sentiment import get_sentiment
+
+KEYWORD_AMOUNT = 6                  # May be empty, better not to
 
 
-def main():
-    DOCUMENT_TO_PROCESS = "001.htm"
-    doc = preprocessing(DOCUMENT_TO_PROCESS)
-    keywords = compute_tfidf(get_corpus(), doc, 5)
-    print("\nAbstract:")
-    print(doc)
-    print("\nKeywords:")
-    for k in keywords:
-        print(k, keywords[k])
+def main():    
+    document_to_process = []   
+    for doc_name in document_to_process:
+        doc = preprocessing(doc_name)
+        keywords = compute_tfidf(get_corpus(), doc, KEYWORD_AMOUNT)
+        sentiment = get_sentiment(doc)
+        print("\nAbstract:")
+        print(doc)
+        print("\nSentiment:")
+        print(sentiment)
+        print("\nKeywords:")
+        for k in keywords:
+            print(k, keywords[k])
+        print("==================")
     return
 
 
